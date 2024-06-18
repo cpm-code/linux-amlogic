4b3221d902bbf9721eff6456ce96a7384d604cc1
cpm <162544069+cpm-code@users.noreply.github.com>
2024-06-18 01:43:37 +0000

    DV Always set DV Enhancement Toggle Flag when HPD plug-in

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
@@ -59,6 +59,7 @@
 #include <linux/amlogic/media/vout/hdmi_tx/hdmi_config.h>
 #include <linux/amlogic/media/vout/hdmi_tx/hdmi_tx_notify.h>
 #include <linux/amlogic/media/vout/hdmi_tx/hdmi_tx_ext.h>
+#include <linux/amlogic/media/amdolbyvision/dolby_vision.h>
 #include "hw/tvenc_conf.h"
 #include "hw/hdmi_tx_reg.h"
 #include "hw/mach_reg.h"
@@ -6193,6 +6194,9 @@ static void hdmitx_hpd_plugin_handler(struct work_struct *work)
 	cancel_delayed_work(&hdev->work_cedst);
 	if (hdev->cedst_policy)
 		queue_delayed_work(hdev->cedst_wq, &hdev->work_cedst, 0);
+
+	// Set toogle flag for DV on plugin.
+	dolby_vision_set_toggle_flag(1);
 }
 
 static void clear_rx_vinfo(struct hdmitx_dev *hdev)
